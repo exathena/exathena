c010f73aedb1837fe58c1a6720290727e0082206
Alexandre de Souza <alexandre@aledsz.com.br>
2022-06-24 12:26:53 -0300

    Improve ip fetching when connecting to socket

--- src/exathena/lib/exathena_web/channels/public_socket.ex
+++ src/exathena/lib/exathena_web/channels/public_socket.ex
@@ -6,11 +6,16 @@ defmodule ExAthenaWeb.PublicSocket do
 
   @impl true
   def connect(params, socket, connect_info) do
-    ip_address = RemoteIp.from(connect_info[:x_headers])
-    ip = ip_address |> :inet.ntoa() |> to_string()
-
-    {:ok, assign(socket, ip: ip, params: params)}
+    {:ok, assign(socket, ip: fetch_ip(connect_info), params: params)}
   end
+
+  defp fetch_ip(%{x_headers: x_headers}) do
+    if ip_address = RemoteIp.from(x_headers) do
+      ip_address |> :inet.ntoa() |> to_string()
+    end
+  end
+
+  defp fetch_ip(_), do: nil
 
   @impl true
   def id(%Phoenix.Socket{assigns: %{ip: ip}}) when ip not in [nil, ""], do: "public:#{ip}"
