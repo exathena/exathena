c36034683b62f4e634378fc55f97b5195988234c
Alexandre de Souza <alexandre@aledsz.com.br>
2026-07-05 18:49:46 -0300

    Rename file

--- lib/exathena/encrypted_types.ex
+++ lib/exathena/encrypted_types.ex
@@ -1,7 +1,4 @@
 defmodule ExAthena.Encrypted.Binary do
-  # This module is a custom data type for encrypting string fields
-  # using `Cloak`.
   @moduledoc false
-
   use Cloak.Ecto.Binary, vault: ExAthena.Vault
 end
--- lib/exathena/vault.ex
+++ lib/exathena/vault.ex
@@ -1,6 +1,4 @@
 defmodule ExAthena.Vault do
-  # It is the app used by `Cloak` to manage encryption.
   @moduledoc false
-
   use Cloak.Vault, otp_app: :exathena
 end
