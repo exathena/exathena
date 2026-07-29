5f2d7a901eae67f3cb796ba8c9ff177b9eaa9ec2
Alexandre de Souza <alexandre@aledsz.com.br>
2026-07-29 12:42:06 -0300

    Remove ExAthenaEvents

--- test/support/mocks.ex
+++ test/support/mocks.ex
@@ -1,13 +1,1 @@
-import Mox
-
-defmock(ExAthena.ClockMock, for: ExAthena.Clock)
-defmock(ExAthenaEventsMock, for: ExAthenaEvents.Behaviour)
-
-defmodule FakeExAthenaEvents do
-  @moduledoc false
-  @behaviour ExAthenaEvents.Behaviour
-
-  def user_authentication_requested(_), do: :ok
-  def user_authentication_accepted(_, _), do: :ok
-  def user_authentication_rejected(_, _), do: :ok
-end
+Mox.defmock(ExAthena.ClockMock, for: ExAthena.Clock)
