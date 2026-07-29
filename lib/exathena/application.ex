71aec3f6d17481997d40e3aebecd85517447cf1c
Alexandre de Souza <alexandre@aledsz.com.br>
2026-07-29 01:45:28 -0300

    Start all workers on app boot

--- lib/exathena/application.ex
+++ lib/exathena/application.ex
@@ -15,45 +15,13 @@ defmodule ExAthena.Application do
         ExAthena.Vault,
         {Registry, keys: :unique, name: ExAthenaMmo.Registry},
         {DynamicSupervisor, strategy: :one_for_one, name: ExAthenaMmo.Client},
-        {Oban, oban}
-      ] ++
-        logger_repo() ++
-        start_configs() ++
-        start_databases()
-
-    :ok = start_handlers()
+        {Oban, oban},
+        ExAthena.Config,
+        ExAthena.Database
+      ]
 
     opts = [strategy: :one_for_one, name: ExAthena.Supervisor]
     Supervisor.start_link(children, opts)
-  end
-
-  cond do
-    ExAthenaLogger.Sql in Application.compile_env(:exathena, :logger_adapters, []) ->
-      defp logger_repo, do: [ExAthenaLogger.Repo]
-
-    Mix.env() == :test ->
-      defp logger_repo, do: [ExAthenaLogger.Repo]
-
-    :else ->
-      defp logger_repo, do: []
-  end
-
-  if Mix.env() == :test do
-    defp start_handlers, do: :ok
-  else
-    defp start_handlers, do: ExAthenaLogger.start_handlers()
-  end
-
-  if Mix.env() == :test do
-    defp start_configs, do: []
-  else
-    defp start_configs, do: [ExAthena.Config]
-  end
-
-  if Mix.env() == :test do
-    defp start_databases, do: []
-  else
-    defp start_databases, do: [ExAthena.Database]
   end
 
   @impl true
